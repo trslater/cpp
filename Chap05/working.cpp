c7993af9e70b70b777dfef875ec67a89bab2f641
Tristan Slater <hello@trslater.ca>
2023-06-16 13:12:39 -0700

    Complete chatper 5 challenge

--- Chap05/working.cpp
+++ Chap05/working.cpp
@@ -1,11 +1,35 @@
 // working.cpp by Bill Weinman [bw.org]
 // updated 2022-05-21
 #include <fmt/core.h>
+#include <vector>
 #include <iostream>
 
 using fmt::format;
 using std::cout;
 
+bool is_prime( int num, std::vector<int>& primes )
+{
+    for( int prime : primes ) if( num % prime == 0 ) return false;
+    
+    return true;
+}
+
 int main() {
-    cout << "Hello, World!\n";
+    const int goal {25};
+    int count = 1;
+    int num = 3;
+    std::vector<int> primes {2};
+
+    while( count < goal )
+    {
+        if( is_prime( num, primes ) ) 
+        {
+            primes.push_back( num );
+            ++count;
+        }
+        
+        ++num;
+    }
+
+    for( int prime : primes ) cout << format( "{}\n", prime );
 }
