7af0b7a757558f1780b2d3e5a6725ea5264d5ae9
Tristan Slater <hello@trslater.ca>
2023-06-26 14:30:18 -0700

    Update missed C++ Essential Training file with fmt

--- cpp-essential-training/Chap09/format.cpp
+++ cpp-essential-training/Chap09/format.cpp
@@ -20,7 +20,7 @@ struct Frac {
 
 // format specialization
 template <typename T>
-struct std::formatter<Frac<T>> : std::formatter<unsigned> {
+struct fmt::formatter<Frac<T>> : fmt::formatter<unsigned> {
     template <typename Context>
     auto format(const Frac<T>& f, Context& ctx) const {
         return format_to(ctx.out(), "{}/{}", f.n, f.d);
@@ -30,7 +30,7 @@ struct std::formatter<Frac<T>> : std::formatter<unsigned> {
 // format print
 template<typename... Args>
 constexpr void print(const std::string_view str_fmt, Args&&... args) {
-    fputs(std::vformat(str_fmt, std::make_format_args(args...)).c_str(), stdout);
+    fputs(fmt::vformat(str_fmt, fmt::make_format_args(args...)).c_str(), stdout);
 }
 
 int main() {
